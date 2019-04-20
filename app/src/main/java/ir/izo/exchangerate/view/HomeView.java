16fca7450a59677f63c9b619fcd2b053ab467b2e
Your Name <you@example.com>
2019-04-20 11:53:59 +0430

    First Version

---
+++ app/src/main/java/ir/izo/exchangerate/view/HomeView.java
@@ -0,0 +1,16 @@
+package ir.izo.exchangerate.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import ir.izo.exchangerate.R;
+
+/**
+ * The home screen that gets name for next usage.
+ */
+public class HomeView extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
